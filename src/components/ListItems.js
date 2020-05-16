c9f68c08aa93487c5554c7d2f991ab6155ff01a0
fatih celik <fhcelik@gmail.com>
2020-05-16 13:41:23 -0600

    add list item

---
+++ src/components/ListItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './list';
+import Search from './search'
+import { Grid } from '@material-ui/core';
+
+export default () => (
+    <Grid>
+        <Search/>
+        <List/>
+    </Grid>
+)
+ 
